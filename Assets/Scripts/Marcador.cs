236ab5d00cf99bdfe315d21d5a164b7df427afbb
BenitoKamelas <adrianramosboira@gmail.com>
2020-02-01 12:21:30 +0100

    Marcadores y Scena

---
+++ Assets/Scripts/Marcador.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Marcador : MonoBehaviour
+{
+    
+    public int timerMeta;
+    public int timerMuerte;
+    private int contador;
+
+    public GameObject atlas;
+    public GameObject obstaculo;
+
+    public CapsuleCollider aCC;
+    public CapsuleCollider oCC;
+
+    // Update is called once per frame
+    void Update()
+    {
+        timerMeta--;
+        timerMuerte--;
+        contador++;
+
+        if(aCC.OnCollisionEnter()){
+            timerMuerte-=10;
+            contador = 0;
+        }
+
+        if(contador == 20)
+        {
+            timerMuerte += 10;
+            contador = 0;
+        }
+
+        if(timerMeta == 0)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+        else if( timerMuerte == 0)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+        }
+    }
+}
