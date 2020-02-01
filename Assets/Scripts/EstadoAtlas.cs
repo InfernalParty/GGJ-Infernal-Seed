dbdc7f54314f38d6a335799dfa26205c59cb6939
Ma1pa <ma1paespana@gmail.com>
2020-02-01 18:50:47 +0100

    Movimiento y rotacion al 90% faltan detener

--- Assets/Scripts/EstadoAtlas.cs
+++ Assets/Scripts/EstadoAtlas.cs
@@ -10,6 +10,8 @@ public class EstadoAtlas : MonoBehaviour
    public Vector3 Movimiento;
    public CapsuleCollider colisionador;
    private Rigidbody cuerpo;
+   private RaycastHit tocado;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -28,14 +30,41 @@ public class EstadoAtlas : MonoBehaviour
         //Fusionar con hugo
 
         //Hacia delante
-        Movimiento = (transform.forward * velocidad);
+        Movimiento = (transform.forward * velocidad);      
 
-        //rotar
-        //cuerpo.MoveRotation(new Quaternion(0, 90, 0, 1));
-        //Rotar Derecha
-        cuerpo.angularVelocity = new Vector3(0,Mathf.PI/2,0);
-        //Rotar Izquierda
-        cuerpo.angularVelocity = new Vector3(0,Mathf.PI/2,0);
+        //Girar
+        Physics.Raycast(transform.position,Vector3.down, out tocado);
+        if(tocado.transform.gameObject != null){
+            if(tocado.transform.gameObject.tag == "Cruce"){
+                    int rotar = tocado.transform.gameObject.GetComponent<Encrucijada>().rotacion;
+                    girando = true;
+                    switch(rotar){
+                        case 1:
+                            //Rotar Derecha
+                            cuerpo.angularVelocity = new Vector3(0,Mathf.PI/2,0);
+                        break;
+                        case 2:
+                            //Rotar Izquierda
+                            cuerpo.angularVelocity = new Vector3(0,-Mathf.PI/2,0);
+                        break;
+                    }
+            }
+            else{
+                girando = false;
+                cuerpo.angularVelocity = Vector3.zero;
+                if(cuerpo.rotation.y <= 45 && cuerpo.rotation.y > -45)
+                    cuerpo.rotation = new Quaternion(cuerpo.rotation.x, 0, cuerpo.rotation.z, cuerpo.rotation.w);
+                else if(cuerpo.rotation.y > 45 && cuerpo.rotation.y <= 135)
+                    cuerpo.rotation = new Quaternion(cuerpo.rotation.x, 90, cuerpo.rotation.z, cuerpo.rotation.w);
+                else if(cuerpo.rotation.y > -135 && cuerpo.rotation.y <= -45)
+                    cuerpo.rotation = new Quaternion(cuerpo.rotation.x, -90, cuerpo.rotation.z, cuerpo.rotation.w);
+                else
+                    cuerpo.rotation = new Quaternion(cuerpo.rotation.x, 180, cuerpo.rotation.z, cuerpo.rotation.w);
+            }
+        }
+
+
+
 
                 //Moverse
         cuerpo.velocity = new Vector3(transform.forward.x * velocidad, cuerpo.velocity.y, transform.forward.z * velocidad);
