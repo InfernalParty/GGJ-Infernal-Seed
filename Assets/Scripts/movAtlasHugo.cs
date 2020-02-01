a506c079a2b737df49a66888d3d5e0a7a80d5eba
Enloch <Hugodlcq@gmail.com>
2020-02-01 16:52:13 +0100

    Prototipado de nivel 0.01

---
+++ Assets/Scripts/movAtlasHugo.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movAtlasHugo : MonoBehaviour
+{
+    public KeyCode moverIzq;
+    public KeyCode moverDer;
+    
+    public float VelHorizontal = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+           GetComponent<Rigidbody> ().velocity = new Vector3(VelHorizontal,0,4);
+        if (Input.GetKeyDown(moverIzq))
+        {
+            VelHorizontal = -2;
+            StartCoroutine(pararMov());
+        }    
+        if (Input.GetKeyDown(moverDer))
+        {
+            VelHorizontal = 2;
+            StartCoroutine(pararMov());
+        } 
+    }
+    IEnumerator pararMov()
+    {
+        yield return new WaitForSeconds(.5f);
+        VelHorizontal = 0;
+    }
+}
