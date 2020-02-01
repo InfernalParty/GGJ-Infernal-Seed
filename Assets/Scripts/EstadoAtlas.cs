10fc3cf355a89a1a1d402294bda5511dba54cb46
Ma1pa <ma1paespana@gmail.com>
2020-02-01 19:48:45 +0100

    Giro completado. aunque no queda exacto

--- Assets/Scripts/EstadoAtlas.cs
+++ Assets/Scripts/EstadoAtlas.cs
@@ -11,6 +11,8 @@ public class EstadoAtlas : MonoBehaviour
    public CapsuleCollider colisionador;
    private Rigidbody cuerpo;
    private RaycastHit tocado;
+   public int contadorGiro = 0;
+   private int rotar;
 
     // Start is called before the first frame update
     void Start()
@@ -34,37 +36,29 @@ public class EstadoAtlas : MonoBehaviour
 
         //Girar
         Physics.Raycast(transform.position,Vector3.down, out tocado);
-        if(tocado.transform.gameObject != null){
+        if(tocado.transform.gameObject != null ){
             if(tocado.transform.gameObject.tag == "Cruce"){
-                    int rotar = tocado.transform.gameObject.GetComponent<Encrucijada>().rotacion;
-                    girando = true;
-                    switch(rotar){
-                        case 1:
-                            //Rotar Derecha
-                            cuerpo.angularVelocity = new Vector3(0,Mathf.PI/2,0);
-                        break;
-                        case 2:
-                            //Rotar Izquierda
-                            cuerpo.angularVelocity = new Vector3(0,-Mathf.PI/2,0);
-                        break;
-                    }
-            }
-            else{
-                girando = false;
-                cuerpo.angularVelocity = Vector3.zero;
-                if(cuerpo.rotation.y <= 45 && cuerpo.rotation.y > -45)
-                    cuerpo.rotation = new Quaternion(cuerpo.rotation.x, 0, cuerpo.rotation.z, cuerpo.rotation.w);
-                else if(cuerpo.rotation.y > 45 && cuerpo.rotation.y <= 135)
-                    cuerpo.rotation = new Quaternion(cuerpo.rotation.x, 90, cuerpo.rotation.z, cuerpo.rotation.w);
-                else if(cuerpo.rotation.y > -135 && cuerpo.rotation.y <= -45)
-                    cuerpo.rotation = new Quaternion(cuerpo.rotation.x, -90, cuerpo.rotation.z, cuerpo.rotation.w);
-                else
-                    cuerpo.rotation = new Quaternion(cuerpo.rotation.x, 180, cuerpo.rotation.z, cuerpo.rotation.w);
+                rotar = tocado.transform.gameObject.GetComponent<Encrucijada>().rotacion;
+                if(!girando)
+                    contadorGiro = 50;
             }
         }
-
-
-
+        if(contadorGiro > 0){
+        girando = true;
+        switch(rotar){
+            case 1:
+                //Rotar Derecha
+                cuerpo.angularVelocity = new Vector3(0,Mathf.PI/2,0);
+            break;
+            case 2:
+                //Rotar Izquierda
+                cuerpo.angularVelocity = new Vector3(0,-Mathf.PI/2,0);
+            break;
+            }
+            contadorGiro--;
+        }
+        else
+            cuerpo.angularVelocity = Vector3.zero;
 
                 //Moverse
         cuerpo.velocity = new Vector3(transform.forward.x * velocidad, cuerpo.velocity.y, transform.forward.z * velocidad);
