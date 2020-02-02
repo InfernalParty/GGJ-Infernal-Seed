f825b6abe663adfac3f951cd2700a1060fa9d1c6
Raven <valverdeballester@gmail.com>
2020-02-02 01:33:14 +0100

    Spawn 99% ready

---
+++ Assets/Scripts/Muevelanza.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Muevelanza : MonoBehaviour
+{
+    public GameObject lanza;
+    public float speed = 30;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject instBullet = Instantiate(lanza, transform.position, Quaternion.identity) as GameObject;
+        Rigidbody instBulletRIgidbody = instBullet.GetComponent<Rigidbody>();
+        instBulletRIgidbody.AddForce(Vector3.forward * speed);
+    }
+}
