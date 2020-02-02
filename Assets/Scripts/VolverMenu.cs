8b93bd9171f5f067f93358d64c397352205b7c64
BenitoKamelas <adrianramosboira@gmail.com>
2020-02-02 17:24:39 +0100

    Fuentes, menus y demas mierdas

---
+++ Assets/Scripts/VolverMenu.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class VolverMenu : MonoBehaviour
+{
+
+    public void Return(){
+        SceneManager.LoadScene(0);
+    }
+
+}
